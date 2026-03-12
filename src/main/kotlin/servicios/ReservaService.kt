1a14245b3fb704055729291ff673b2b95e0cbf13
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-03-12 19:49:52 +0100

    Añadidos métodos creación de instancias

--- src/main/kotlin/servicios/ReservaService.kt
+++ src/main/kotlin/servicios/ReservaService.kt
@@ -1,8 +1,18 @@
 package servicios
 
+import dominio.Reserva
+import dominio.ReservaHotel
+import dominio.ReservaVuelo
+
 class ReservaService(private val repositorio : IReservaRepository) {
 
-    fun crearVuelo(descripcion: String, origen: String, destino: String, hora: String): Reserva {
+    fun crearVuelo(descripcion: String, fechaCreacion : String,origen: String, destino: String, hora: String): Reserva {
+        val reservaVuelo = ReservaVuelo.crearInstancia(descripcion, fechaCreacion, origen, destino,destino,hora)
+        return reservaVuelo
+    }
 
+    fun crearHotel(id : String, fechaCreacion : String, descripcion : String, ubicacion : String, numeroNoches : Int) : Reserva{
+        val reservaHotel = ReservaHotel.crearInstancia(id, fechaCreacion, descripcion, ubicacion, numeroNoches)
+        return reservaHotel
     }
 }

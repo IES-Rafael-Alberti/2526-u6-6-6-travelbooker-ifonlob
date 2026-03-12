e503a00c12e6657e991be705e865e5a8f5628e4b
ifonlob <ifonlob1108@g.educaand.es>
2026-03-12 10:52:21 +0100

    Implementación ReservaRespositoryImpl.kt

--- src/main/kotlin/datos/ReservaRespositoryImpl.kt
+++ src/main/kotlin/datos/ReservaRespositoryImpl.kt
@@ -1,6 +1,20 @@
 package datos
 
 import dominio.Reserva
+import servicios.IReservaRepository
 
-class ReservaRespositoryImpl<T : Reserva> {
+class ReservaRespositoryImpl : IReservaRepository {
+    private val reservas : MutableList<Reserva> = mutableListOf()
+
+    override fun agregar(reserva: Reserva) {
+        reservas.add(reserva)
+    }
+
+    override fun obtenerTodas(): List<Reserva> {
+        val listadoReservas : MutableList<Reserva> = mutableListOf()
+        reservas.forEach{
+            listadoReservas.add(it)
+        }
+        return listadoReservas
+    }
 }

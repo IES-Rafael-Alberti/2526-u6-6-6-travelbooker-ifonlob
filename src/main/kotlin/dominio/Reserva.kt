23f6371bdb0a83dd0e5fe9780c49169d56b9c4d5
ifonlob <ifonlob1108@g.educaand.es>
2026-03-13 10:47:25 +0100

    Formato ANSI fecha arreglado

--- src/main/kotlin/dominio/Reserva.kt
+++ src/main/kotlin/dominio/Reserva.kt
@@ -1,13 +1,24 @@
 package dominio
 
-abstract class Reserva(val id : String = Reserva.generarId().toString(), val fechaCreacion = , val descripcion : String){
+import java.time.LocalDate
+import java.time.ZoneId
 
+abstract class Reserva(val id : String = Reserva.generarId().toString(), val fechaCreacion : String = Reserva.generarFecha(), val descripcion : String){
+    init{
+        val fechaRegex = """\d{4}-\d{2}-\d{2}""".toRegex()
+        require(fechaRegex.containsMatchIn(fechaCreacion))
+    }
     companion object{
         private var contadorId = 0
 
         fun generarId() : Int{
             contadorId++
             return contadorId
+        }
+
+        fun generarFecha() : String{
+            val fechaAnsi =  LocalDate.now(ZoneId.systemDefault()).toString()
+            return fechaAnsi
         }
     }
 

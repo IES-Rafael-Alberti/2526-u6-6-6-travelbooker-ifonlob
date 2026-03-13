accb0547582536cb2c63effa926509d116d9365b
ifonlob <ifonlob1108@g.educaand.es>
2026-03-13 13:19:29 +0100

    Añadida lógica de adicción a repositorio

--- src/main/kotlin/servicios/ReservaService.kt
+++ src/main/kotlin/servicios/ReservaService.kt
@@ -8,20 +8,20 @@ class ReservaService(private val repositorio : IReservaRepository) {
 
     fun crearVuelo(origen: String, destino: String, hora: String): Reserva {
         val reservaVuelo = ReservaVuelo.crearInstancia(origen, destino,hora)
+        repositorio.agregar(reservaVuelo)
         return reservaVuelo
     }
 
-    fun crearHotel(id : String, fechaCreacion : String, descripcion : String, ubicacion : String, numeroNoches : Int) : Reserva{
+    fun crearHotel(ubicacion : String, numeroNoches : Int) : Reserva{
         val reservaHotel = ReservaHotel.crearInstancia(ubicacion, numeroNoches)
+        repositorio.agregar(reservaHotel)
         return reservaHotel
     }
+
+    fun obtenerReservas() = repositorio.obtenerTodas()
 
     fun buscarPorId(id: String) : Reserva?{
         val reserva = repositorio.obtenerTodas().find{it.id == id}
         return reserva
-    }
-
-    fun añadirReserva(reserva: Reserva){
-        repositorio.agregar(reserva)
     }
 }

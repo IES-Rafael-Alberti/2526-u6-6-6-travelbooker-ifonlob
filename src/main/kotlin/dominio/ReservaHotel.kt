6497a808fa963260d5d13d50f094b759d1b8e39d
ifonlob <ifonlob1108@g.educaand.es>
2026-03-13 12:37:29 +0100

    Añadida descripción de prueba por defecto

--- src/main/kotlin/dominio/ReservaHotel.kt
+++ src/main/kotlin/dominio/ReservaHotel.kt
@@ -3,7 +3,7 @@ package dominio
 class ReservaHotel private constructor(id : String, fechaCreacion : String,descripcion : String, val ubicacion : String, val numeroNoches : Int) : Reserva(id,fechaCreacion,descripcion){
 
     companion object{
-        fun crearInstancia(descripcion : String, ubicacion : String, numeroNoches : Int) = ReservaHotel(id = generarId(),fechaCreacion = generarFecha(),descripcion,ubicacion,numeroNoches)
+        fun crearInstancia(ubicacion : String, numeroNoches : Int) = ReservaHotel(id = generarId(),fechaCreacion = generarFecha(),descripcion = "descripcion-test",ubicacion,numeroNoches)
     }
     override val detalle : String
         get() = "$id - $descripcion - $ubicacion($numeroNoches)"
--- src/main/kotlin/dominio/ReservaVuelo.kt
+++ src/main/kotlin/dominio/ReservaVuelo.kt
@@ -1,19 +1,19 @@
 package dominio
 
-class ReservaVuelo private constructor(id : String, fechaCreacion: String, descripcion: String,val origen : String, val destino : String, val horaVuelo : String) : Reserva(id,fechaCreacion,descripcion){
+class ReservaVuelo private constructor(id : String, fechaCreacion: String, descripcion : String,val origen : String, val destino : String, val horaVuelo : String) : Reserva(id,fechaCreacion,descripcion){
     init{
         val horaRegex = "^([01][0-9]|2[0-3]):[0-5][0-9]$".toRegex()
         require(horaRegex.containsMatchIn(horaVuelo))
     }
     companion object{
-        fun crearInstancia(descripcion : String, origen : String, destino : String, horaVuelo : String) = ReservaVuelo(id = generarId() , fechaCreacion = generarFecha(),descripcion, origen, destino, horaVuelo)
+        fun crearInstancia(origen : String, destino : String, horaVuelo : String) = ReservaVuelo(id = generarId() , fechaCreacion = generarFecha(),descripcion = "descripcion-test", origen, destino, horaVuelo)
     }
 
     override val detalle : String
         get() = "$id - $descripcion - $origen -> $destino [$horaVuelo]"
 
     override fun toString() : String{
-        return "Reserva con id $id creada el $fechaCreacion con descripción $(descripcion).\nOrigen: $origen Destino: $destino Hora vuelo: $horaVuelo"
+        return "Reserva con id $id creada el $fechaCreacion con descripción (${descripcion}).\nOrigen: $origen Destino: $destino Hora vuelo: $horaVuelo"
     }
 
 }

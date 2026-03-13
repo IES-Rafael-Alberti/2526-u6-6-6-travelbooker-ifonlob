cc229f7d4adf1ae486e86981b1460d60678cb2ac
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-03-13 16:36:34 +0100

    Main del programa añadido

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
-import datos.ReservaRespositoryImpl
 import servicios.IReservaRepository
 import presentacion.ConsolaReserva
+import servicios.ReservaService
+import datos.ReservaRepositoryImpl
 
 fun main() {
     val repositorio: IReservaRepository = ReservaRepositoryImpl()
     val servicio = ReservaService(repositorio)
     val consola = ConsolaReserva(servicio)
 
-    consola.ejecutar() // ← Lanza el programa
+    consola.ejecutar()
 }

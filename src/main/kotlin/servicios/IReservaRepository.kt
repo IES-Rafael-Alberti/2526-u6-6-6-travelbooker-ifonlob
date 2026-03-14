4760d5af11f4176ec44fe095a8611ab758270d6d
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-03-14 10:18:09 +0100

    Documentación hecha

--- src/main/kotlin/servicios/IReservaRepository.kt
+++ src/main/kotlin/servicios/IReservaRepository.kt
@@ -1,8 +1,21 @@
 package servicios
 
 import dominio.Reserva
-
+/**
+ * Interfaz repositorio. Abstracción para aplicar DIP.
+ * La lógica negocio depende de esta interfaz, no de implementación concreta.
+ */
 interface IReservaRepository {
+    /**
+     * Almacena nueva reserva usando su ID como clave.
+     *
+     * @param reserva Instancia Reserva a guardar
+     */
     fun agregar(reserva: Reserva)
+    /**
+     * Recupera todas las reservas almacenadas.
+     *
+     * @return Lista completa de reservas
+     */
     fun obtenerTodas(): List<Reserva>
 }
